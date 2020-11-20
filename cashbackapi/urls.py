62a38114dc42c1ad53388dfbc24b89a022c3fba9
silviolleite <silviolleite@gmail.com>
2020-11-19 22:28:43 -0300

    Add schema view

--- cashbackapi/urls.py
+++ cashbackapi/urls.py
@@ -1,6 +1,13 @@
 from django.urls import include, path
-from django.contrib import admin
+from rest_framework.schemas import get_schema_view
+
 
 urlpatterns = [
     path('api/', include("cashbackapi.apps.cashback.urls")),
+    path('', get_schema_view(
+            title="Cashback API",
+            description="API for cashback control",
+            version="1.0.0"
+        ), name='openapi-schema'),
 ]
+
